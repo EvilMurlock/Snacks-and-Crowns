1be8ffb68f9b49797a3145de1aac787231faa7ba
Samuel Juraj Koprda <koprda.samuel@gmail.com>
2025-05-25 21:44:55 +0200

    Large bakalarka rewrite
    The docx file got a bit too big to back up, but I realized I can just back up the pdf alone.

--- Snacks and Crowns/Assets/Resources/Scripts/Race.cs
+++ Snacks and Crowns/Assets/Resources/Scripts/Race.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+
 [CreateAssetMenu(fileName = "New_Race", menuName = "Race")]
 public class Race : ScriptableObject
 {
